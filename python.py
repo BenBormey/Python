6d8ebc5259485baf38ac1c2e74b6232e3d13e368
BenBormey <boemry902@gmail.com>
2025-10-21 10:05:39 +0700

    modify

---
+++ python.py
@@ -0,0 +1,58 @@
+def add(a, b):
+    return a + b
+print(add(5, 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Enter your name: ")
+
+# age  = input("Enter your age: ")
+# if(int(age) < 18):
+#     print("Age cannot be negative.")
+# else:
+#     print(f"Hello {name}  are {age} years old ?")
+
+
+# arr = ["apple", "banana", "cherry"]
+# for x in arr: 
+#     print(x)
+
+# def greet(name):
+#     return f"Hello {name}"
+
+# print(greet("Bormey"))
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello from Flask!'
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# print(9000)
